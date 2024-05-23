614864f5a12b24934c8d2cce9fb66955992068c0
sebas cueto <cuetosalazarsebas@gmail.com>
2024-05-23 05:07:39 -0500

    alerta-component

--- Desarrollo/Uywa/Codigo/Backend/uywa-backend/src/alertas/alertas.controller.ts
+++ Desarrollo/Uywa/Codigo/Backend/uywa-backend/src/alertas/alertas.controller.ts
@@ -1,23 +1,41 @@
-import { Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, UsePipes, ValidationPipe } from '@nestjs/common';
 import { AlertasService } from './alertas.service';
+import { AlertasAuthDto } from './dto/AlertasAuth.dto';
 import { LocalAuthGuard } from 'src/auth-user/jwt-auth.guard';
+import { AlertaFechaLugarDto } from './dto/AlertaFechaLugar.dto';
+import { reporte } from '@prisma/client';
 
 //esta webada es el que me manda en todas las rutas
 //la puedo cambiar por un objeto vacio para definir mis propias rutas
-@Controller('alertas')
+@Controller('/alertas')
 export class AlertasController {
   
     
     constructor ( private alertasService: AlertasService){}
-    @UseGuards(LocalAuthGuard)
-    @Get('/')
-    getAllAlertas() {
-        return this.alertasService.getAlertas();
+
+    @UsePipes(new ValidationPipe)
+    @Post('/guardar')
+    async saveAlerta(@Body() body: AlertasAuthDto){
+        return this.alertasService.createAlerta(body);
     }
 
-    @Post('/')
-    saveAlerta(){
-        return 'Save alerta';
+    @Get('/allmap')
+    async getall(){
+        return this.alertasService.getLocations();
     }
+
+    @Get('/basic')
+    async getBasic(){
+        return this.alertasService.getBasic();
+    }
+
+    @Get('/search')
+    getAlertaByFechaAndDireccion(
+        @Query() alerta : AlertaFechaLugarDto
+    ) {
+        return this.alertasService.getAlertaByFechaAndDireccion(alerta);
+    }
+
+    
 
 }

43c3c035dfda1253b3de2840241a6f91506d2aba
Daniel Lorenzo <danielalorenzoramos@gmail.com>
2024-06-05 21:46:11 -0500

    Fix Charter

--- Desarrollo/Uywa/Codigo/Frontend/react-uywa/src/components/CharterModerador/Charter.js
+++ Desarrollo/Uywa/Codigo/Frontend/react-uywa/src/components/CharterModerador/Charter.js
@@ -14,10 +14,10 @@ const Charter = ({opcion, colorFondo, IconComponent, ruta})=> {
     <Link to={ruta} style={{textDecoration: 'none'}}>
     <Card sx={{backgroundColor:colorFondo}} className="charter-card">
     <Box sx={{ position: "relative", display: "flex", justifyContent: "center", alignItems: "center", height: "150px" }}>
-          <IconComponent sx={{ fontSize: 80, color: 'white' }} />
+          <IconComponent sx={{ fontSize: 150, color: 'white' }} />
         </Box>
       <CardContent className="charter-cardContent">
-        <Typography variant="body2" color="text.secondary" className="charter-typography">
+        <Typography variant="body2" color="text.secondary" sx={{fontSize:"25px", color:"white", fontWeight:"bold", fontFamily:"Arial Black"}}>
             {opcion}
         </Typography>
       </CardContent>
--- Desarrollo/Uywa/Codigo/Frontend/react-uywa/src/pages/Moderador.js
+++ Desarrollo/Uywa/Codigo/Frontend/react-uywa/src/pages/Moderador.js
@@ -11,24 +11,24 @@ function ModeradorPrincipal() {
     return (
         <div className="Menu">
             <Container  sx={{display: 'flex', flexDirection: 'column', alignItems: 'center', marginBottom:"15vh"}}>
-                <div style={{height: 'auto', position:"relative"}}>
+                <Box className="moderador-container-image">
                     <CardMedia
                         component="img"
                         alt="InnovaTech"
                         height="600"
                         image="https://source.unsplash.com/random?wallpapers"
-                        sx={{width: '100vw'}}
+                        className="moderador-image"
                     />
-                    <Typography variant="h1" className="charter-title">
+                    <Typography variant="h1" className="moderador-title">
                         Bienvenido Moderador
                     </Typography>
-                </div>
+                </Box>
                 <Card sx={{textAlign:'center', backgroundColor:"#D3D3D3", width:"80vw", margin:"5%"}}>
-                    <Typography variant="h4" sx={{textAlign:'center', padding:3}}>
+                    <Typography variant="h4" sx={{textAlign:'center', padding:3, fontSize:"clamp(0.8rem, 2vw, 2rem)"}}>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                     </Typography>
                 </Card>
-                <Box className="charter-box">
+                <Box className="moderador-charter-box">
                     <Charter opcion="Lista de Alertas" colorFondo="#1ec48c" IconComponent={image1} ruta="/ver-alerta"/>
                     <Charter opcion="Gestionar Eventos" colorFondo="#1ec48c" IconComponent={image2} ruta="/ver-alerta"/>
                     <Charter opcion="Promover Usuario" colorFondo="#E02200" IconComponent={image3} ruta="/ver-alerta"/>

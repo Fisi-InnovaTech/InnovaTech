b4c09fc4713e2e100dd1e57a93ee0cca261b5453
Mateo_choy <119556627+vizcacha22@users.noreply.github.com>
2024-06-20 15:01:32 -0500

    test insignia
    creacion de las relaciones y correcta implementacion del test para insignias

---
+++ Desarrollo/Uywa/Codigo/Backend/uywa-backend/src/insignias/tests/app.insignias.spec.ts
@@ -0,0 +1,4 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { InsigniasController } from '../insignias.controller';
+import { InsigniasService } from '../insignias.service';
+import { PrismaService } from 'src/prisma/prisma.service';

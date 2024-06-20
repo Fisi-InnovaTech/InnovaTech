05fb5b8f7aa972679e723770447139af7b954aad
Mateo_choy <119556627+vizcacha22@users.noreply.github.com>
2024-06-20 15:02:25 -0500

    descripcion del test
    Definimos que vamos a testear

--- Desarrollo/Uywa/Codigo/Backend/uywa-backend/src/insignias/tests/app.insignias.spec.ts
+++ Desarrollo/Uywa/Codigo/Backend/uywa-backend/src/insignias/tests/app.insignias.spec.ts
@@ -2,3 +2,12 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { InsigniasController } from '../insignias.controller';
 import { InsigniasService } from '../insignias.service';
 import { PrismaService } from 'src/prisma/prisma.service';
+
+
+describe('InsigniasController', () => {
+  let insigniasController: InsigniasController;
+  let insigniasService: InsigniasService;
+  let prismaService: PrismaService;
+
+  
+});

a29880dda73bbc28291f781a368e330b7e01a257
Mateo_choy <119556627+vizcacha22@users.noreply.github.com>
2024-07-04 02:44:10 -0500

    Contribuciones computadora

--- Desarrollo/Uywa/Codigo/Frontend/react-uywa/src/pages/Profile.js
+++ Desarrollo/Uywa/Codigo/Frontend/react-uywa/src/pages/Profile.js
@@ -158,7 +158,7 @@ const Profile = () => {
             </Grid>
             <Grid item xs={12}>
               <Paper style={profileStyles.infoPaper}>
-                <Typography variant="body1"><strong>Contribuciones:</strong> 120</Typography>
+                <Typography variant="body1"><strong>Contribuciones:</strong> 0</Typography>
               </Paper>
             </Grid>
             <Grid item xs={12}>

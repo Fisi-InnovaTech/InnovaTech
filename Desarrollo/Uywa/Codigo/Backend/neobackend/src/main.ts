df19cf5dcb4b4a3e6aee80ce78db9824013536e3
Yayir <yayir.mattos@unmsm.edu.pe>
2025-11-24 01:20:17 -0500

    Fix: Reports by id

--- Desarrollo/Uywa/Codigo/Backend/neobackend/src/main.ts
+++ Desarrollo/Uywa/Codigo/Backend/neobackend/src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
   const config = new DocumentBuilder()
     .setTitle('Uywa Backend API')
     .setDescription(
--- Desarrollo/Uywa/Codigo/Backend/neobackend/src/reportes/dto/find.one-reporte.dto.ts
+++ Desarrollo/Uywa/Codigo/Backend/neobackend/src/reportes/dto/find.one-reporte.dto.ts
@@ -1,9 +1,14 @@
 import { 
     IsInt, 
-    Min 
+    Min,
 } from 'class-validator';
 
+import {
+    Type
+} from 'class-transformer';
+
 export class FindOneReporteDto {
+  @Type(() => Number)
   @IsInt({ message: 'El ID debe ser un número entero' })
   @Min(1, { message: 'El ID debe ser mayor a 0' })
   id: number;
